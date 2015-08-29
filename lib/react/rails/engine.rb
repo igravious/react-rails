b36256d1feba5c4e1cff5e0119bdf0d4a0191e3f
Anthony Durity <anthony@durity.com>
2015-08-29 16:47:44 +0100

    Track Sprockets 4.x API

--- lib/react/rails/engine.rb
+++ lib/react/rails/engine.rb
@@ -2,8 +2,9 @@ module React
   module Rails
     class Engine < ::Rails::Engine
       initializer "react_rails.setup_engine", :group => :all do |app|
-        sprockets_env = app.assets || Sprockets # Sprockets 3.x expects this in a different place
-        sprockets_env.register_engine(".jsx", React::JSX::Template)
+        sprockets_env = app.assets || Sprockets # Totally not sure about the Sprockets 4.x way :(
+        sprockets_env.register_mime_type 'text/jsx', extensions: ['.jsx', '.js.jsx']
+        sprockets_env.register_transformer 'text/jsx', 'application/vnd.facebook.javascript+xml', React::JSX::Template
       end
     end
   end
